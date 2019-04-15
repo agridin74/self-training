334697c3f01f5e6c3a23d010aa97e25063b2a96e
Alex Gridin <agridin74@gmail.com>
2019-04-15 09:12:08 +0300

    nio

---
+++ complete8shil/ExplicitChannelWrite.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.nio.file.*;
+
+public class ExplicitChannelWrite {
+	public static void main(String args[]){
+		try(FileChannel fChan = (FileChannel)
+			Files.newByteChannel(Paths.get("test.txt"),
+					  StandardOpenOption.WRITE,
+					  StandardOpenOption.CREATE  ))
+		{
+			ByteBuffer mBuf = ByteBuffer.allocate(26);
+			for(int i=0; i<26; i++)
+					  mBuf.put((byte)('A' + i));
+			mBuf.rewind();
+			fChan.write(mBuf);
+		}catch(InvalidPathException e){
+			System.out.println("Err path " + e);
+		}catch(IOException e){
+			System.out.println("Err read-write " + e);
+			System.exit(1);
+		}
+	}
+}

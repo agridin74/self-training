4681d2d3ca059fe730ab45bc10e859fb836e50e5
Alex Gridin <agridin74@gmail.com>
2019-05-28 16:19:53 +0300

    concurent

---
+++ complete8shil/ProdCon.java
@@ -0,0 +1,62 @@
+import java.util.concurrent.Semaphore;
+
+class Q {
+	int n;
+	static Semaphore semCon = new Semaphore(0);
+	static Semaphore semProd = new Semaphore(1);
+
+	void get(){
+		try{
+			semCon.acquire();
+
+		}catch(InterruptedException e){
+			System.out.println("exception "+ e);
+		}
+		System.out.println(" Get: "+n);
+		semProd.release();
+	}
+
+	void put(int n){
+		try{
+			semProd.acquire();
+		}catch(InterruptedException e){
+			System.out.println("exception "+e);
+		}
+
+		this.n = n;
+		System.out.println("Put: "+n);
+		semCon.release();
+	}
+}
+
+class Producer implements Runnable{
+	Q q;
+	Producer(Q q) {
+		this.q = q;
+		new Thread(this, "Producer").start();
+	}
+
+	public void run(){
+		for(int i=0; i<20 ; i++) q.put(i);
+	}
+}
+
+class Consumer implements Runnable{
+	Q q;
+	Consumer(Q q){
+		this.q = q;
+		new Thread(this, "Consumer").start();
+	}
+
+	public void run(){
+		for(int i=0; i<20; i++) q.get();
+	}
+}
+
+class ProdCon {
+	public static void main(String args[]){
+		Q q =  new Q();
+		new Consumer(q);
+		new Producer(q);
+	}
+}
